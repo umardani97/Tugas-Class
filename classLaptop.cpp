a12fa9adbda651bd97a4c96ad8d5ebee238fbaee
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-01-20 16:50:24 +0800

    isi data

--- classLaptop.cpp
+++ classLaptop.cpp
@@ -7,3 +7,23 @@ class Laptop{
 	void spec(string Ghz);
 	string nama, cpu;
 };
+
+int main{
+	Laptop Asus, Acer;
+	
+	Acer.nama = "Laptop Acer E14";
+	Acer.cpu  = "core i7-4510U";
+	Asus.nama = "Laptop Asus";
+	Asus.cpu  = "core i7 i7-7700HQ";
+	
+	cout << (Acer.nama);
+	cout << (Acer.cpu);
+	Acer.spec("2.0Ghz");
+	
+	cout << (Asus.nama);
+	cout << (Asus.cpu);
+	Asus.spec("3.0Ghz");
+	
+}
+
+
